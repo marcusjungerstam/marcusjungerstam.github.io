3cc4f3b545faf01b8ffdcc8cbcfefbb27c65d2f5
marcusjungerstam <35814707+marcusjungerstam@users.noreply.github.com>
2020-10-28 15:14:35 +0100

    new script

--- js/script.js
+++ js/script.js
@@ -168,7 +168,7 @@ $(document).ready(function(){
 	setTimeout( function(){
 		if($("#site-type").length) {
         $(".typewrite span").typed({
-            strings: ["hey","HE", "Hey!" "Welcome"],
+            strings: ["hey","HE", "Hey!", "Welcome"],
             typeSpeed: 100,
             backDelay: 500,
             loop: false,
