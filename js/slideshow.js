32a82145549dd58e140a0abcbb8c1e3ad641a0cc
marcusjungerstam <marcusjungerstam@gmail.com>
2024-12-08 17:06:44 +0100

    logo 2
    a big logo upfdate

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,23 +1,18 @@
 var i = 0;
-var images = [];
+var images = [
+    'img/project_6/img_1.png',
+    'img/project_6/img_3.png',
+    'img/project_6/img_6.png',
+];
+
 var time = 3000;
 
-images[0] = 'img/project_6/img_6.png';
-images[1] = 'img/project_6/img_2.png';
-images[2] = 'img/project_6/img_5.png';
-
-images[3] = 'img/project_2/img_1.png';
-images[4] = 'img/project_2/img_4.png';
-images[5] = 'img/project_2/img_8.png';
 
 
-images[6] = 'img/project_4/img_2.png';
-images[7] = 'img/project_4/img_3.png';
-images[8] = 'img/project_4/img_4.png';
-
+const slide=document.getElementById("slide")
 
 function changeImg(){
-    document.slide.src = images[i];
+    slide.src = images[i];
 
     if(i < images.length-1){
         i++
@@ -27,7 +22,7 @@ function changeImg(){
         i = 0
     }
 
-    setTimeout("changeImg()", time)
+    setTimeout(changeImg,time)
 }
 
 
