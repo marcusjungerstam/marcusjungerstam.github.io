faa7a594c3ff2970e68cbffd3d447fea8f56eef5
marcusjungerstam <marcusjungerstam@gmail.com>
2024-12-29 16:01:33 +0100

    index new
    index new

---
+++ js/slideshow_index_desktop.js
@@ -0,0 +1,59 @@
+var i = 0;
+var screenWidth = window.innerWidth;
+var desktop_images = [
+    'img/index/landscape/img_6.png',
+    'img/index/landscape/img_7.png',
+    'img/index/landscape/img_2.png',
+    'img/index/landscape/img_1.png',
+    'img/index/landscape/img_2.png',
+    'img/index/landscape/img_3.png',
+    'img/index/landscape/img_4.png',
+    'img/index/landscape/img_2.png',
+    'img/index/landscape/img_8.png',
+    'img/index/landscape/img_9.png',
+];
+
+var portrait_images = [
+    'img/index/portrait/img_1.png',
+    'img/index/portrait/img_2.png',
+    'img/index/portrait/img_3.png',
+    'img/index/portrait/img_4.png',
+    'img/index/portrait/img_5.png',
+    'img/index/portrait/img_6.png',
+    'img/index/portrait/img_7.png',
+    
+];
+
+
+
+var time = 3000;
+const slide=document.getElementById("slide")
+
+
+
+
+function changeImg(){
+
+    
+    if(screenWidth > 600){var img = desktop_images}
+    else{var img = portrait_images}
+    
+
+    
+    
+    slide.src = img[i];
+
+    if(i < img.length-1){
+        i++
+    } 
+    else {
+        i = 0
+    }
+    setTimeout(changeImg,time)
+
+}
+
+
+
+
+window.onload = changeImg;
